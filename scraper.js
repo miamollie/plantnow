85f40e835f5244f176c9d33310865fe77effc928
miamollie <mia.deburca@99designs.com>
2020-07-14 22:03:27 +1000

    Install JS and save scraped data to file

--- scraper.js
+++ scraper.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
+const fs = require("fs");
 
 const baseUrl = "https://www.abc.net.au";
 const zoneUrls = [
@@ -10,7 +11,7 @@ const zoneUrls = [
   "https://www.abc.net.au/gardening/cool-months/10018040",
 ];
 
-const getData = async function () {
+const getData = function () {
   const climateData = {};
   const plantData = {};
   zoneUrls.forEach(async function (url) {
@@ -19,8 +20,10 @@ const getData = async function () {
     let html = response.data;
     let $ = cheerio.load(html);
     let climateZone = $("#main-content h1").text().trim();
-    climateData[climateZone] = [];
     let months = $("#comp-rich-text7 p");
+    if (!(climateZone in climateData)) {
+      climateData[climateZone] = [];
+    }
 
     months.each(async function () {
       let node = $(this).find("a");
@@ -33,7 +36,10 @@ const getData = async function () {
         let html = plantsResp.data;
         let $ = cheerio.load(html);
         let plants = $("#comp-rich-text5 p");
-        climateData[climateZone][monthTag] = [];
+        if (!(monthTag in climateData[climateZone])) {
+          climateData[climateZone][monthTag] = [];
+        }
+
         plants.each(async function () {
           let node = $(this).find("a");
           let plantUrl = node.attr("href");
@@ -41,35 +47,53 @@ const getData = async function () {
             let plantDataResp = await axios(
               `${baseUrl}${plantUrl}`
             ).catch((e) => console.log(e));
-            let html = plantDataResp.data;
-            let $ = cheerio.load(html);
-            let name = $("[name*='commonname'] + p").text();
-            let botanicalName = $("[name*='botanicalname'] + p").text();
-            let hint = $("[name*='harvest'] + p").text();
-            let harvest = $("[name*='hints'] + p").text();
-            climateData[climateZone][monthTag].push(name);
-            plant = {
-              name,
-              botanicalName,
-              hint,
-              harvest,
-              plantUrl,
-            };
-            if (!plantData[name]) {
-              plantData[name] = plant;
-              console.log(plantData);
+            if (plantDataResp) {
+              let html = plantDataResp.data;
+              let $ = cheerio.load(html);
+              let name = $("[name*='commonname'] + p").text();
+              let botanicalName = $("[name*='botanicalname'] + p").text();
+              let hint = $("[name*='harvest'] + p").text();
+              let harvest = $("[name*='hints'] + p").text();
+              let imgUrl = $("figure img").attr("src");
+
+              climateData[climateZone][monthTag].push(name);
+
+              plant = {
+                name,
+                botanicalName,
+                hint,
+                harvest,
+                plantUrl,
+                imgUrl,
+              };
+              if (!plantData[name]) {
+                plantData[name] = plant;
+                fs.writeFile(
+                  "plants.json",
+                  JSON.stringify(plantData),
+                  function (err) {
+                    if (err) {
+                      return console.log(err);
+                    }
+                  }
+                );
+
+                fs.writeFile(
+                  "data.json",
+                  JSON.stringify(climateData),
+                  function (err) {
+                    if (err) {
+                      return console.log(err);
+                    }
+                  }
+                );
+              }
             }
           }
         });
       }
     });
   });
-  return [climateData, plantData];
 };
 
-const scrapeData = async function () {
-  const d = await getData().catch((e) => console.log(e));
-  console.log(d);
-};
-
-scrapeData();
+getData();
