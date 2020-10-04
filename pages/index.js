338a1123edf450ebae700c49c12ed050298171a7
miamollie <mia.deburca@99designs.com>
2020-10-05 10:45:19 +1100

    Start styling with MUI

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
 import useSWR from "swr";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
 
 const GQL_API = "/api/graphql";
 
@@ -38,22 +41,28 @@ export default function Index() {
   }
 
   const { name, plants } = data.climate;
+  const season = "spring"; //todo dynamic
 
+  //TODO split out a layout component
   return (
-    <main>
-      <h1>{`We've detected you're in: ${name}`}</h1>
+    <Container component="main" maxWidth="md">
+      <Typography variant="h2" component="h1" gutterBottom align="center">
+        What should I plant now?
+      </Typography>
+      <Typography variant="p" component="p" gutterBottom>
+        {`Looks like it's ${season} time and your're in the ${name.toLowerCase()}, here are some things
+        you could plant now:`}
+      </Typography>
       <section>
-        <div>
-          {plants
-            ? plants.map((p, i) => (
-                <article key={i}>
-                  {p.name}
-                  <img src={p.imgUrl} role="presentational" alt="" />
-                </article>
-              ))
-            : "no plants..."}
-        </div>
+        {plants
+          ? plants.map((p, i) => (
+              <Box component="article" key={i}>
+                {p.name}
+                <img src={p.imgUrl} role="presentational" alt="" />
+              </Box>
+            ))
+          : "Sorry, we couldn't find any plants for you this time"}
       </section>
-    </main>
+    </Container>
   );
 }
